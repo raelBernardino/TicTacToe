11a039920818c3a5d35216bf6c6d806970aa6d99
Rael Bernardino <bernardino1207@gmail.com>
2019-05-19 20:24:07 -0700

    Updates

---
+++ precache-manifest.a289c3b9816a0d0012be0fead3999ce6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9862e24375faaa6eb5505d2f7511e86f",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "7635985d7caf9bc8d4e4",
+    "url": "/TicTacToe/static/css/main.6ae45cd8.chunk.css"
+  },
+  {
+    "revision": "7fd118425e980766f823",
+    "url": "/TicTacToe/static/js/2.21a9cdf6.chunk.js"
+  },
+  {
+    "revision": "7635985d7caf9bc8d4e4",
+    "url": "/TicTacToe/static/js/main.14c6a9d0.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
